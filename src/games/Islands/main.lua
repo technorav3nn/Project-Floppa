6e1bc792b1e0a6c3793087a4bb1e048e51e6b74c
Colin Donaghy <colin01460@outlook.com>
2022-11-20 18:00:08 -0500

    initialize Islands

---
+++ src/games/Islands/main.lua
@@ -0,0 +1,7 @@
+local LibModule = require("modules/exploit/ui/Vynixius")
+
+local Library, Window = LibModule.createVynixiusLib("Project Floppa", "Islands")
+
+Window:AddTab("Farming")
+
+LibModule.initalizeSettingsTab(Window)
--- src/main.lua
+++ src/main.lua
@@ -8,4 +8,6 @@ if game.PlaceId == 7860844204 then
     require("games/LifeSentence/main")
 elseif game.PlaceId == 606849621 then
     require("games/Jailbreak/main")
+elseif game.PlaceId == 4872321990 then
+    require("games/Islands/main")
 end
