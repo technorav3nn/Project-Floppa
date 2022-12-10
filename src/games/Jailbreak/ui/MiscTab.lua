a929cce1e4714d88f2518f8ce661ce0736a285bb
technorav3nn <Colin01460@outlook.com>
2022-12-10 18:24:35 -0500

    fix error with gun

--- src/games/Jailbreak/ui/MiscTab.lua
+++ src/games/Jailbreak/ui/MiscTab.lua
@@ -1,23 +1,3 @@
-local Gun = require(ReplicatedStorage.Game.Item.Gun)
-
--- // create a sniper to pop tires and disable helis
-local trollSniper = {
-    __ClassName = "Sniper",
-    LastImpactSound = 1,
-    Maid = require(ReplicatedStorage.Module.Maid),
-    LastImpact = 1,
-    Local = true,
-    Config = {},
-    IgnoreList = {},
-}
-
--- // "make" it into a gun
-Gun.SetupBulletEmitter(trollSniper)
-
-trollSniper.OnHitSurface:Connect(function()
-    print("sniper hit surface unknown if car or heli")
-end)
-
 local function miscTab()
 
 end
